baf46d6481f0c128dbe735e788b4a91d9738f79b
Sandor Teleki <sandorteleki1@gmail.com>
2026-06-06 14:31:42 +0200

    feat: prepping for agent to be able to call functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -45,7 +45,20 @@ if args.verbose:
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if response.function_calls:
-    for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
+    function_results = []
+    for fc in response.function_calls:
+        function_call_result = call_function(fc, verbose=args.verbose)
+
+        if not function_call_result.parts:
+            raise RuntimeError("Function call result has no parts")
+        if function_call_result.parts[0].function_response is None:
+            raise RuntimeError("Function call result has no function_response")
+        if function_call_result.parts[0].function_response.response is None:
+            raise RuntimeError("Function call result has no response")
+
+        function_results.append(function_call_result.parts[0])
+
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 else:
     print(response.text)
